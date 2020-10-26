e925313204241e5d09dfb9de784625b52d3c34b6
Dominik1860 <73120285+Dominik1860@users.noreply.github.com>
2020-10-26 18:24:14 +0100

    Admin and Models

--- libook/users/admin.py
+++ libook/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import User, Comment, Feed, Access_Type
+
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Feed)
+admin.site.register(Access_Type)
--- libook/users/models.py
+++ libook/users/models.py
@@ -2,3 +2,64 @@ from django.db import models
 
 # Create your models here.
 
+class User(models.Model):
+    id = models.DecimalField()
+    email = models.EmailField()
+    username = models.CharField(max_length=15)
+    password = models.CharField(max_length=15)
+    name = models.CharField(max_length=15)
+    surname = models.CharField(max_length=15)
+    birthdate = models.DateField()
+    telephone = models.DecimalField(max_digits=12)
+
+    def __init__(self, id, email, username, password, name, surname, birthdate, telephone):
+        self.id = id
+        self.email = email
+        self.username = username
+        self.password = password
+        self.name = name
+        self.surname = surname
+        self.birthdate = birthdate
+        self.telephone = telephone
+
+        def age(self):
+            today = datetime.date.today()
+            age = today.year - self.birthdate.year
+
+            if today < datetime.date(today.year, self.birthdate.month, self.birthdate.day):
+                age -= 1
+
+            return age
+        # calculates the age of user
+
+
+class Comment(models.Model):
+    id = models.DecimalField()
+    # user_id
+    # post_id
+    content = models.TextField(max_length=120)
+
+    def __init__(self, id, user_id, post_id, content):
+        self.id = id
+        self.user_id = user_id
+        self.post_id = post_id
+        self.content = content
+
+
+class Feed(models.Model):
+    id = models.DecimalField()
+    # user_id
+    # post_id
+    access_type = models.TextField(max_length=120)
+
+    def __init__(self, id, user_id, post_id, content):
+        self.id = id
+        self.user_id = user_id
+        self.post_id = post_id
+        self.access_type = access_type
+
+
+# from enum import Enum
+class Access_Type(Enum):
+    private = 1
+    public = 2
