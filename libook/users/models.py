153dcaf494cfb88bb3426d94f0226a9c6b278b06
Dominik1860 <73120285+Dominik1860@users.noreply.github.com>
2020-10-29 15:04:12 +0100

    Models Post, Reaction, Reaction Type and Post Type

--- libook/users/models.py
+++ libook/users/models.py
@@ -23,10 +23,17 @@ class AccessTypes(IntEnum):
 #     IMAGE = 1, ('Image')
 #     VIDEO = 2, ('Video')
 
+class ReactionTypeEnum(IntEnum):
+    """
+    Enumeration for defining the reaction to a post
+    """
+    LIKE = 0, ('Like')
+    SHARE = 1, ('Share')
+
 
 class User(models.Model):
     """
-    Model class for an users
+    Model class for an user
     """
 
     # def __init__(self, id, email, username, password, name, surname, birthdate, telephone, *args, **kwargs):
@@ -61,15 +68,35 @@ class User(models.Model):
 
 class Comment(models.Model):
     """
-    Model class for a comment from a particular users (1:1) to a particular post (1:1)
+    Model class for a comment from a particular user (1:1) to a particular post (1:1)
     """
     user_id = models.ForeignKey('user', on_delete=models.CASCADE)
     post_id = models.ForeignKey('post', on_delete=models.CASCADE)
     content = models.TextField(null=False, blank=False, max_length=255)
 
-    def __init__(self, id, user_id, post_id, content, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.id = id
-        self.user_id = user_id
-        self.post_id = post_id
-        self.content = content
+    # def __init__(self, id, user_id, post_id, content, *args, **kwargs):
+    #    super().__init__(*args, **kwargs)
+    #    self.id = id
+    #    self.user_id = user_id
+    #    self.post_id = post_id
+    #    self.content = content
+
+class Post (models.Model):
+    """
+    Model class for a post from a particular user (1:1)
+    """
+    name = models.CharField(max_length=15)
+    description = models.TextField(null=False, blank=False, max_length=255)
+    # content_url = models.
+    number_of_likes = models.DecimalField()
+    tagged_user_id = models.ForeignKey('user', on_delete=models.CASCADE)
+    post_type_id = models.ForeignKey('PostTypeEnum', on_delete=models.CASCADE)
+    privacy_settings = models.TextField(choices=AccessTypes.choices(), default=AccessTypes.PRIVATE)
+
+
+class Reaction (models.Model):
+    """
+    Model class for a reaction from a particular user (1:1) to a particular post (1:1)
+    """
+    post_id = models.ForeignKey('post', on_delete=models.CASCADE)
+    reaction_type_id = models.ForeignKey('ReactionTypeEnum', on_delete=models.CASCADE)
