465820e12764555ed11d4133819cf98657b97fbe
leonard nduwayo <73165057+leonardNduwayo@users.noreply.github.com>
2020-11-10 00:38:30 +0100

    Update urls.py

--- libook/libook/urls.py
+++ libook/libook/urls.py
@@ -19,3 +19,17 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
