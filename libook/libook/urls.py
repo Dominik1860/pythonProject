b5b19f24654e0a3fb308265f5e5c2822b48dd4aa
leonard nduwayo <73165057+leonardNduwayo@users.noreply.github.com>
2020-11-10 09:49:14 +0100

    view

--- libook/libook/urls.py
+++ libook/libook/urls.py
@@ -15,19 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-from django.urls import path
-
-from . import views
-
 urlpatterns = [
     path('', views.index, name='index'),
 ]
-from django.contrib import admin
-from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
--- libook/posts/views.py
+++ libook/posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
 # Create your views here.
+
+def home_views(*args, **kwargs):
+    return HttpResponse("<h1>welcome to my web !</h1>""")
+
+class IndexView(View):
+    def post(self, request, *args, **kwargs):
+        return HttpResponse("views")
+
+
+def comment(self,request,*args, **kwargs):
+   return HttpResponse("<h1> my post!</h1>")
+
+
