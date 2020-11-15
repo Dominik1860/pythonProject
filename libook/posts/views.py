c4c9c5ab76439bfd89a7f2e2a810df1d71dc7b2b
lukas-plojhar <57949123+lukas-plojhar@users.noreply.github.com>
2020-11-15 14:09:27 +0100

    Sunday call
    example of implementing a view

--- libook/posts/views.py
+++ libook/posts/views.py
@@ -6,5 +6,16 @@ from .models import Post
 from . import forms
 
 
+def detail(request, **kwargs):
+    post_id = kwargs['id']
+    # post = Post.objects.get(pk=post_id)
+
+    context = {
+        'id' : post_id,
+        'test': 'test',
+        'test_number': 1
+    }
+
+    return render(request, 'post/detail.html', context)
 
 
