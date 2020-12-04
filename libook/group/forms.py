de19031f047222f54df798953e6eea7c43b003c9
Dominik1860 <73120285+Dominik1860@users.noreply.github.com>
2020-12-04 13:30:24 +0100

    New Feature 'Group' added 2/

--- libook/group/forms.py
+++ libook/group/forms.py
@@ -1,2 +1,26 @@
 from django import forms
 from .models import Group
+
+class CreateGroupForm(forms.ModelForm):
+    """
+    Form for creating a new group entity
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        https://stackoverflow.com/questions/31627253/django-modelform-with-bootstrap
+        """
+        super(CreateGroupForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+        self.fields['user'].widget.attrs['class'] = 'form-control user-hidden-field'
+
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
--- libook/group/urls.py
+++ libook/group/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/new', views.CreateNewGroupView.as_view(), name='group_new'),
+    path('/new', views.create_new.as_view(), name='create_new'),
     path('/detail/<int:pk>', views.GroupDetailView.as_view(), name='group_detail')
 ]
