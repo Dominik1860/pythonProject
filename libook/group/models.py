e89e91ddfd1dd6e4f0f1ff2555c2615ae602efb4
Dominik1860 <73120285+Dominik1860@users.noreply.github.com>
2020-12-04 13:49:27 +0100

    New Feature 'Group' added 3/

---
+++ libook/group/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth import settings
+from profiles.models import Profile
+from posts.models import Post
+
+
+class Group(models.Model):
+    """
+    Group model class
+    """
+    admin = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, default=None, null=True, blank=True)
+    feed = models.ManyToManyField('posts.post', on_delete=models.CASCADE, default=None, null=True, blank=True)
+    #feed = models.ManyToManyField('self', symmetrical=True)
+    bio = models.TextField(max_length=255, null=True, blank=True)
+    genre = models.TextField(max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to='static/imgs/profile/', default='static/imgs/dummy_avatar.jpg')
+
+
+class InvitationRequest(models.Model):
+    """
+    Model class for an invitation request from a particular user (1:1) to a particular group (1:1)
+    """
+
+    to_group = models.ForeignKey('group.group', related_name='to_group', on_delete=models.CASCADE)
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='from_user', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "From {}, to {}".format(self.from_user.username, self.to_group.groupname)
