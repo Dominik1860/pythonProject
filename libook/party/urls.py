eaa3b8a71e1b260a183eb6f1c2573ba8816556af
Dominik1860 <73120285+Dominik1860@users.noreply.github.com>
2020-12-13 17:26:07 +0100

    Changes to party/urls.py

--- libook/party/urls.py
+++ libook/party/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create', views.create_new, name='create_party'),
-    path('detail/<int:pk>', views.PartyDetailView.as_view(), name='detail_party')
+    path('all/', views.PartyListView.as_view(), name='list_party'),
+    path('myevents/', views.PartyUserFilteredListView.as_view(), name='my_party'),
+    path('create/', views.PartyCreateView.as_view(), name='create_party'),
+    path('update/<int:pk>', views.PartyUpdateView.as_view(), name='update_party'),
+    path('detail/<int:pk>', views.PartyDetailView.as_view(), name='detail_party'),
+    path('signup/', views.signup, ),
+    path('unsubscribe/', views.unsubscribe, ),
 ]
